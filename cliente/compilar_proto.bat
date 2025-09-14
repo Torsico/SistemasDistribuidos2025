2ec9e742591b1b4a0c7c634486091c021cf9d3ec
Torsico <167933591+Torsico@users.noreply.github.com>
2025-09-14 18:27:01 -0300

    agrega proto + script compilador prueba

---
+++ cliente/compilar_proto.bat
@@ -0,0 +1,2 @@
+:: grpc_tools_ruby_protoc -I ../../protos --ruby_out=../lib --grpc_out=../lib ../../protos/route_guide.proto
+grpc_tools_ruby_protoc -I protos --ruby_out=lib --grpc_out=lib protos/cosas.proto
