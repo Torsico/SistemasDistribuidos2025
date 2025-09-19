7b8d10d689dccb7a2b2b1c96718d5d71b34cf9fc
Andres <31314529+Andres1097@users.noreply.github.com>
2025-09-19 20:15:29 -0300

    Update Server & Usuario.proto

--- servidor/server.py
+++ servidor/server.py
@@ -24,10 +24,15 @@ class UsuarioServiceImpl(usuarios_pb2_grpc.UsuarioServiceServicer):
         for u in usuariosBD:
             lista.usuarios.append(
                 usuarios_pb2.Usuario(
-                    nombreUsuario=u[0],
-                    nombre=u[1],
-                    apellido=u[2],
-                    email=u[3],
+                    idusuario=u[0],
+                    nombreUsuario=u[1],
+                    nombre=u[2],
+                    apellido=u[3],
+                    email=u[4],
+                    rol=u[5],
+                    clave=u[6],
+                    telefono=u[7],
+                    activo=bool(u[8])
                 )
             )
         return lista
