ead613e172147ef75e3650f8ce5dfafb5c516390
Andres <31314529+Andres1097@users.noreply.github.com>
2025-09-20 00:39:50 -0300

    Alta, Mod y Baja de Usuario en Server

--- servidor/bdConnect.py
+++ servidor/bdConnect.py
@@ -1,9 +1,6 @@
 import mysql.connector
 
 def get_connection():
-    """
-    Crea y devuelve una conexión a la base de datos MySQL.
-    """
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -12,6 +9,8 @@ def get_connection():
     )
     return conn
 
+# Consultas de Usuario
+
 def get_usuarios():
     conn = get_connection()
     cursor = conn.cursor()
@@ -19,6 +18,53 @@ def get_usuarios():
     rows = cursor.fetchall()
     conn.close()
     return rows
+
+def alta_usuario(nombreUsuario, nombre, apellido, email, rol, clave, telefono, activo):
+    try:
+        conn =  get_connection()
+        cursor = conn.cursor()
+        sql = "INSERT INTO usuarios (nombreUsuario, nombre, apellido, email, rol, clave, telefono, activo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (nombreUsuario, nombre, apellido, email, rol, clave, telefono, activo)
+        cursor.execute(sql, values)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return True
+    except Exception as e:
+        print("Error al cargar")
+        return False
+    
+def mod_usuario(idusuario, nombreUsuario, nombre, apellido, email, rol, clave, telefono, activo):
+    try:
+        conn =  get_connection()
+        cursor = conn.cursor()
+        sql = "UPDATE usuarios SET nombreUsuario = %s, nombre = %s, apellido = %s, email = %s, rol = = %s, telefono = %s, activo = %s WHERE idusuario = %s"
+        values = (nombreUsuario, nombre, apellido, email, rol, clave, telefono, activo)
+        cursor.execute(sql, values)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return True
+    except Exception as e:
+        print("Error al cargar")
+        return False
+    
+def baja_usuario(idusuario):
+    try:
+        conn =  get_connection()
+        cursor = conn.cursor()
+        sql = "UPDATE usuarios SET activo = false WHERE idusuario = %s"
+        values = (idusuario)
+        cursor.execute(sql, values)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return True
+    except Exception as e:
+        print("Error al cargar")
+        return False
+
+# ...
 
 def get_roles():
     conn = get_connection()
--- servidor/server.py
+++ servidor/server.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Agregar la carpeta 'proto' al path
+# Se agrega la carpeta 'proto' al path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "proto"))
 
 from concurrent import futures
@@ -9,12 +9,10 @@ import logging
 
 import grpc
 
-from bdConnect import get_usuarios
+from bdConnect import get_usuarios, alta_usuario, mod_usuario, baja_usuario
 from bdConnect import get_roles
-from proto import usuarios_pb2
-from proto import usuarios_pb2_grpc
-from proto import rol_pb2
-from proto import rol_pb2_grpc
+from proto import usuarios_pb2, usuarios_pb2_grpc
+from proto import rol_pb2, rol_pb2_grpc
 
 
 class UsuarioServiceImpl(usuarios_pb2_grpc.UsuarioServiceServicer):
@@ -36,6 +34,41 @@ class UsuarioServiceImpl(usuarios_pb2_grpc.UsuarioServiceServicer):
                 )
             )
         return lista
+    
+    def AltaUsuario(self, request, context):
+        usuario = request.usuario
+
+        exito = alta_usuario(
+            usuario.nombreUsuario,
+            usuario.nombre,
+            usuario.apellido,
+            usuario.email,
+            usuario.rol,
+            usuario.clave,      # Modificar luego para que se autogenere la clave
+            usuario.telefono,
+            usuario.activo
+        )
+        return usuarios_pb2.AltaUsuarioResponse(suceso=exito)
+    
+    def ModUsuario(self, request, context):
+        usuario = request.usuario
+
+        exito = mod_usuario(
+            usuario.nombreUsuario,
+            usuario.nombre,
+            usuario.apellido,
+            usuario.email,
+            usuario.rol,
+            usuario.clave,
+            usuario.telefono,
+            usuario.activo
+        )
+        return usuarios_pb2.ModUsuarioResponse(suceso=exito)
+    
+    def BajaUsuario(self, request, context):
+        usuario = request.usuario
+        exito = baja_usuario(usuario)
+        return usuarios_pb2.BajaUsuarioResponse(suceso=exito)
 
 
 class RolServiceImpl(rol_pb2_grpc.RolServiceServicer):
