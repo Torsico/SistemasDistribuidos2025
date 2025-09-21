cdc7064dacf08bddcaa715abe8957e0df2e9eef4
Torsico <167933591+Torsico@users.noreply.github.com>
2025-09-21 01:58:56 -0300

    agrega inv/ev boilerplate + rutas

---
+++ cliente/app/controllers/eventos_controller.rb
@@ -0,0 +1,22 @@
+require 'clases' # no es necesario gracias a Rails, pero permite recargar
+
+require 'grpc'
+require 'google/protobuf/empty_pb'
+
+class InventariosController < ApplicationController
+    
+    @@stub = nil
+    
+    before_action do
+        unless @@stub # reusar stub en vez de crear uno nuevo
+            #@@stub = Session::LoginService::Stub.new('localhost:50051', :this_channel_is_insecure)
+        end
+    end
+    
+    def root
+        # cosas
+        # flash[:noticetext] = ":)"
+        # flash[:noticecolor] = @@color_info
+    end
+end
+  
--- cliente/config/routes.rb
+++ cliente/config/routes.rb
@@ -1,32 +1,48 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+	# Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+	# Can be used by load balancers and uptime monitors to verify that the app is live.
+	get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+	# Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+	# get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+	# get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+	# Defines the root path route ("/")
+	# root "posts#index"
 
-  root "root#root"
-  
-  #resources(...) enruta:
-  # #index #new #show #edit (todos GET)
-  # #create #update #destroy (POST PATCH/PUT DELETE)
-  #resources :usuarios
-  
-  get "login" => "root#login"
-  post "login" => "root#loginpost"
-  post "logout" => "root#logoutpost"
-  
-  get "usuarios" => "usuarios#index"
-  get "usuarios/alta" => "usuarios#altaform"
-  get "usuarios/mod/:id" => "usuarios#modform"
-  post "usuarios/alta" => "usuarios#altapost"
-  post "usuarios/mod/:id" => "usuarios#modpost"
-  
+	root "root#root"
+	
+	#resources(...) enruta:
+	# #index #new #show #edit (todos GET)
+	# #create #update #destroy (POST PATCH/PUT DELETE)
+	#resources :usuarios
+	
+	get "login"		=> "root#login"
+	post "login"	=> "root#loginpost"
+	post "logout"	=> "root#logoutpost"
+	
+	get "usuarios"			=> "usuarios#index"
+	get "usuarios/alta"		=> "usuarios#altaform"
+	get "usuarios/mod/:id"	=> "usuarios#modform"
+	post "usuarios/alta"	=> "usuarios#altapost"
+	post "usuarios/mod/:id"	=> "usuarios#modpost"
+	
+	get "inventarios"			=> "inventario#index"
+	get "inventario/alta"		=> "inventario#altaform"
+	get "inventario/mod/:id"	=> "inventario#modform"
+	get "inventario/baja/:id"	=> "inventario#bajaform"
+	post "inventario/alta"		=> "inventario#altapost"
+	post "inventario/mod/:id"	=> "inventario#modpost"
+	post "inventario/baja/:id"	=> "inventario#bajapost"
+	
+	get "eventos"			=> "eventos#index"
+	get "eventos/alta"		=> "eventos#altaform"
+	get "eventos/mod/:id"	=> "eventos#modform"
+	get "eventos/baja/:id"	=> "eventos#bajaform"
+	post "eventos/alta"		=> "eventos#altapost"
+	post "eventos/mod/:id"	=> "eventos#modpost"
+	post "eventos/baja/:id"	=> "eventos#bajapost"
+	
 end
